10791a497c41ef5a947efd1bb03132dd1c417f0b
sevadp <45359075+sevadp@users.noreply.github.com>
2019-04-18 19:53:55 +0400

    readme

--- main.py
+++ main.py
@@ -100,7 +100,8 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    # время UTC ну лондонское
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 
